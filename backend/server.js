8c78661b8abf01f3be2d5a3b5a706cc9cf82ac40
RebecaTI <mebuffbeka@gmail.com>
2025-08-27 10:30:41 -0300

    refactor: reorganize product API routes for better clarity

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({}) //Ao passar objeto vazio, isso significa q vai fetch todos os produtos dentro do database
+    res.status(200).json({ success: true, data: products })
+  } catch (error) {
+    console.log("Error fetching products:", error.message)
+    res.status(500).json({ success: false, message: "Server Error" })
+  }
+})
+
 app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data
 
@@ -27,7 +37,17 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-// Postman dekstop APP
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await Product.findByIdAndDelete(id)
+    res.status(200).json({ success: true, message: "Product deleted successfully" })
+  } catch (error) {
+    console.log("Error in deleting product:", error.message)
+    res.status(404).json({ success: false, message: "Product not found" })
+  }
+})
 
 app.listen(5000, () => {
   connectDB();
