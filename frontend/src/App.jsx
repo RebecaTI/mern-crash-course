7202105ee570ed21d0feacb14100f4d5b15856ba
RebecaTI <mebuffbeka@gmail.com>
2025-09-02 17:04:51 -0300

    feat: initialize frontend project with Vite, React, and Chakra UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Box, useColorModeValue } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import CreatePage from "./pages/CreatePage"
+import HomePage from "./pages/CreatePage"
+import NavBar from "./components/Navbar"
+
+function App() {
+  return (
+    <Box minHeight="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  )
+}
+
+export default App
