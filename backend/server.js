bb73158cb51e575ede32f3eb0d95fdbf8b32d91a
RebecaTI <mebuffbeka@gmail.com>
2025-09-07 08:39:11 -0300

    feat: update API route for products and enhance static file serving in production

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,15 @@ const PORT = process.env.PORT || 5000
 const __dirname = path.resolve()
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use("/api/products", productRoutes)
+app.use("/api/xxx", productRoutes)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+
 }
 
 app.listen(PORT, () => {
